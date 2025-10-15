c90030962afd13ecab11444e56192a4a8858a302
Ethan Eldridge-Smith <e.eldridge04.ees@gmail.com>
2025-10-15 15:00:32 +0100

    feat: Created interfaces for the Components and systems of the ECS

---
+++ CustomECS/ECS.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomECS.ECS
+{
+    // IComponent - Lets the ECS know which types are valid components
+    public interface IComponent { }
+
+    // ISystem - Ensures that all systems have an Update method and updated in a consistent way
+    public interface ISystem
+    {
+        void Update(World world, float deltaTime);
+    }
+
+
+}
